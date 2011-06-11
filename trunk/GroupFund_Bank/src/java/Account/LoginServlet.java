7159c48654a7051d236595064839dca76326e29d
mms901niit@gmail.com <mms901niit@gmail.com@7ec9d51b-39f0-0e5b-520f-36ce9bf188bb>
2011-06-11 09:05:07 +0000

    Minor update

--- trunk/GroupFund_Bank/src/java/Account/LoginServlet.java
+++ trunk/GroupFund_Bank/src/java/Account/LoginServlet.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Session;
+package Account;
 
 import Ultilities.Hash;
 import java.io.IOException;
